116a8358c3e9e9b84ac6cb699126b23e9900fe23
love.cosma <williamsmatthew333@gmail.com>
2021-09-25 18:20:10 -0600

    Additional styling on about me.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./AboutMe.css"
 import us from "../us.JPG"
 import meGuitar from "../me-guitar.jpeg"
-import meSamara from "../me-samara.jpeg"
+import grad from "../LSUgraduation.jpeg"
 import sa from "../sa.png"
 import bone from "../bone.png"
 export const AboutMe = (props) => {
@@ -15,19 +15,20 @@ export const AboutMe = (props) => {
                         <img className="us-photo" src={us} alt="Matthew's family" width="425px" height="550px"/>
                         <div className="about-me-smaller-photos">
                             <img src={meGuitar} alt="Matthew on guitar" width="200px" height="200x"/>
-                            <img src={meSamara} alt="Matthew with Samara" width="200px" height="200x"/>
+                            <img src={grad} alt="Matthew with Samara" width="200px" height="200x"/>
                         </div>
                     </div>
                     <div className='about-me-text-container'>
                        <div>
+                            <h3> My story actually begins at my birth in Wiesbaden, Hesse, Germany.</h3>
                             <p>
-                                My story starts on Wiesbaden, Hesse, Germany. My father was stationed there in the military at the time.
+                                My father was stationed there in the military at the time. Traveling long distances would become a theme in my life.
                                 After leaving Germany at 2 years old, my family relocated back my parents' hometown of New Orleans, LA in the US.
                                 Over the course of the next 16 years, I would live in places from Louisiana, to Virginia, to Georgia, to Hawaii.
                             </p>
                             <p>
                                 At and early age, I developed an interest for music which ignited into a 
-                                full passion as I grew older. This passionled me to LSU's brand new Experimental Music and Digital
+                                full passion as I grew older. This passion led me to LSU's brand new Experimental Music and Digital
                                 Media Programming for Art and Engineering program. It was here that I cultivated 
                                 a taste for coding a problem solving. My team and I exhibited a game, "Synth Arena", 
                                 we created together using Unity (video game engine) and github at Baton Rouge's 
@@ -35,22 +36,29 @@ export const AboutMe = (props) => {
                                 Overall, I learned the value of open communication and preserverence in the face of complex goals as a team.
                             </p> 
                        </div>
-                        <div style={{
-                            width: "300px",
-                            height: "200px",
-                            backgroundPosition: "center",
-                            backgroundRepeat: "no-repeat",
-                            backgroundSize: "cover",
-                            backgroundImage: `url(${sa})`
-                        }}/>
-                        <div style={{
-                            width: "300px",
-                            height: "200px",
-                            backgroundPosition: "center",
-                            backgroundRepeat: "no-repeat",
-                            backgroundSize: "cover",
-                            backgroundImage: `url(${bone})`
-                        }}/>           
+                       <div className="lsu-project-container">
+                           <div className='lsu-projects' >
+                                <div style={{
+                                    width: "300px",
+                                    height: "200px",
+                                    backgroundPosition: "center",
+                                    backgroundRepeat: "no-repeat",
+                                    backgroundSize: "cover",
+                                    backgroundImage: `url(${sa})`
+                                }}/>
+                                <div style={{
+                                    width: "300px",
+                                    height: "200px",
+                                    backgroundPosition: "center",
+                                    backgroundRepeat: "no-repeat",
+                                    backgroundSize: "cover",
+                                    backgroundImage: `url(${bone})`
+                                }}/> 
+                           </div>
+                           <div>
+
+                           </div>
+                    </div>          
                     </div>
                 </div>
         </section>
