657ab3c5fef4753f1d01e2902c194a897908ba4b
love.cosma <williamsmatthew333@gmail.com>
2021-09-27 10:49:05 -0600

    Continuing with about me section.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,9 +7,11 @@ import sa from "../sa.png"
 import bone from "../bone.png"
 export const AboutMe = (props) => {
     return (
+        <div>
         <section className="about-me">
-                <div className="center">
-                </div>
+                    <div className="center about-me-header">
+                        <h2>About Me</h2>
+                    </div>
                 <div className="upper-about-me">
                     <div className="us-container">
                         <img className="us-photo" src={us} alt="Matthew's family" width="425px" height="550px"/>
@@ -23,7 +25,7 @@ export const AboutMe = (props) => {
                             <h3> My story actually begins at my birth in Wiesbaden, Hesse, Germany.</h3>
                             <p>
                                 My father was stationed there in the military at the time. Traveling long distances would become a theme in my life.
-                                After leaving Germany at 2 years old, my family relocated back my parents' hometown of New Orleans, LA in the US.
+                                After leaving Germany at 2 years old, my family relocated back my parents' hometown of New Orleans, LA.
                                 Over the course of the next 16 years, I would live in places from Louisiana, to Virginia, to Georgia, to Hawaii.
                             </p>
                             <p>
@@ -44,7 +46,8 @@ export const AboutMe = (props) => {
                                     backgroundPosition: "center",
                                     backgroundRepeat: "no-repeat",
                                     backgroundSize: "cover",
-                                    backgroundImage: `url(${sa})`
+                                    backgroundImage: `url(${sa})`,
+                                    boxShadow: "0 0 5px 10px black"
                                 }}/>
                                 <div style={{
                                     width: "300px",
@@ -58,9 +61,23 @@ export const AboutMe = (props) => {
                            <div>
 
                            </div>
-                    </div>          
+                        </div>          
                     </div>
                 </div>
         </section>
+        <section className="about-me" style={{marginTop: "125px"}}>
+            <div className="lower-about-me">
+               <div className="about-me-text-2">
+                   <h2>Continued</h2>
+                   <p>With some text</p>
+                </div> 
+                <div className="about-me-pictures-2 center">
+                    <h2>Hello</h2>
+
+                </div>
+            </div>
+
+        </section>
+    </div>
     )
 }
