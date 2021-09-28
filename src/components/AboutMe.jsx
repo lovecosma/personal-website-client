ecca05bc1edd21b63b03b22e87ab84d0dbbe2071
love.cosma <williamsmatthew333@gmail.com>
2021-09-28 10:54:40 -0600

    Just about done with the about me section for desktop.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -41,18 +41,18 @@ export const AboutMe = ({history}) => {
                        <div>
                             <h3> My story actually begins at my birth in Wiesbaden, Hesse, Germany.</h3>
                             <p>
-                                My father was stationed there in the military at the time. Traveling long distances would become a theme in my life.
-                                After leaving Germany at 2 years old, my family relocated back my parents' hometown of New Orleans, LA.
-                                Over the course of the next 16 years, I would live in places from Louisiana, to Virginia, to Georgia, to Hawaii.
+                                My father was stationed there in the military at the time. Traveling long distances would become a theme in my life. After 
+                                leaving Germany at 2 years old, my family relocated back to my parents' hometown of New Orleans, LA. Over the next 16 years, 
+                                I would live in Louisiana, Virginia,  Georgia, and Hawaii.
                             </p>
                             <p>
-                                At and early age, I developed an interest for music which ignited into a 
-                                full passion as I grew older. This passion led me to LSU's brand new Experimental Music and Digital
-                                Media Programming for Art and Engineering program. It was here that I cultivated 
-                                a taste for coding a problem solving. I learned the basics of object oriented programming, embedded hardware, and experimental sound synthesis.
-                                In 2017, my team exhibited a game, "Synth Arena", we created together using Unity (video game engine) and github at Baton Rouge's 
-                                Red Stick Festival. I was also awarded Best Music and Sound Design by LSU's Digital Art and Design Association for a production entitled "The Bone" for LSU's 72-hr Animation Jam of 2017.
-                                Through these projects, I learned that anything is possible with team work and preserverence in the face of complex goals.
+                                At an early age, I developed an interest in music which ignited into a full passion as I grew older. 
+                                This passion led me to LSU's brand new Experimental Music and Digital Media Programming for Art and Engineering program. 
+                                It was here that I cultivated a taste for coding a problem-solving. I learned the basics of object-oriented programming, 
+                                embedded hardware, and experimental sound synthesis. In 2017, my team exhibited a game, Synth Arena, which we created together 
+                                using Unity (video game engine) and GitHub at Baton Rouge's Red Stick Festival. I was also awarded Best Music and Sound Design by 
+                                LSU's Digital Art and Design Association for a production entitled "The Bone" for LSU's 72-hr Animation Jam of 2017. 
+                                Through these projects, I learned that anything is possible with teamwork and perseverance in the face of complex goals.
                             </p> 
                        </div>
                        <div className="lsu-project-container">
