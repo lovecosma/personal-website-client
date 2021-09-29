6e7ca927a8939ae84f851c467e075be8ad12aeed
love.cosma <williamsmatthew333@gmail.com>
2021-09-29 11:42:57 -0600

    Editted myapps margin

--- src/desktopComponents/MyApps.jsx
+++ src/desktopComponents/MyApps.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export  const MyApps = (props) => {
     return (
-        <div style={{padding: "15px"}}>
+        <div style={{padding: "15px", marginTop: "100px"}}>
             <h2>My Apps</h2>
             <h3>More Content Coming soon!</h3>
             <p style={{fontSize: "x-large"}} >Check out my repo <a href="https://github.com/lovecosma/personal-website-client" alt="repo" style={{fontSize: "x-large"}}>here</a></p>
