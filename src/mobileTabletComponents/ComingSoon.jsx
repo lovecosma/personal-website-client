1719fa662e11b9faee317f1020a2f3d303e2d8fe
love.cosma <williamsmatthew333@gmail.com>
2021-09-28 18:01:43 -0600

    Added sidenav for mobile, accounting for development in deploy

---
+++ src/mobileTabletComponents/ComingSoon.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ComingSoon() {
+    return (
+        <div  className="container white-text" style={{padding: "15px"}}>
+        <p>Responsive Design in progress. Please visit on desktop for now.</p>
+        <p style={{fontSize: "x-large"}} >Check out my repo <a href="https://github.com/lovecosma/personal-website-client" alt="repo" style={{fontSize: "x-large"}}>here</a></p>
+        <p>Updated daily.</p>
+        <div className="center">
+        <a href="https://giphy.com/gifs/wirdesign-typography-wip-workinprogress-TOhTGM1xJ77bZwnNCy"><iframe alt="Coming Soon" title="Coming Soon" src="https://giphy.com/embed/TOhTGM1xJ77bZwnNCy" width="200" height="200" frameBorder="0" class="giphy-embed"></iframe></a>
+        </div>
+        </div>
+    )
+}
--- src/mobileTabletComponents/MobileTabletRouter.jsx
+++ src/mobileTabletComponents/MobileTabletRouter.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Intro from "./Intro"
-import {AboutMe} from "./AboutMe"
-import {MyApps} from "./MyApps"
+// import {AboutMe} from "./AboutMe"
+// import {MyApps} from "./MyApps"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import {NavBar} from "./NavBar"
-
+import ComingSoon from "./ComingSoon"
 export const MobileTabletRouter = () => {
     return (
         <div className="container white-text">
@@ -12,8 +12,8 @@ export const MobileTabletRouter = () => {
                 <NavBar/>
                 <Switch>
                     <Route exact path="/" render={(routerProps)=> <Intro {...routerProps} /> } />
-                    <Route path="/aboutme" render={(routerProps) => <AboutMe {...routerProps} />} />
-                    <Route path="/myapps" component={MyApps} />
+                    <Route path="/aboutme" render={(routerProps) => <ComingSoon {...routerProps}/> } />
+                    <Route path="/myapps" component={ComingSoon} />
                 </Switch>
             </Router>
         </div>
